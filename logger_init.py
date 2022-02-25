4ed8ebd9abe45a99d0773af9a320b3d1eb0dd5ab
hanchang2021 <hanc@dp.tech>
2022-02-25 15:00:03 +0800

    docs(logger_init.py): add comment of logger_init

--- logger_init.py
+++ logger_init.py
@@ -3,6 +3,18 @@ import sys, os
 
 class CustomFormatter(logging.Formatter):
     def format(self, record):
+        """
+        The CustomFormatter class is a subclass of the Formatter class. 
+        It overrides the format() method of the Formatter class. 
+        It adds the func_name_override and file_name_override attributes to the LogRecord object. 
+        It then calls the format() method of the Formatter class
+        
+        Args:
+          record: the record to be formatted.
+        
+        Returns:
+          A string.
+        """
         if hasattr(record, 'func_name_override'):
             record.funcName = record.func_name_override
         if hasattr(record, 'file_name_override'):
@@ -10,6 +22,17 @@ class CustomFormatter(logging.Formatter):
         return super(CustomFormatter, self).format(record)
 
 def logger_init(name: str="", logdir="", level=logging.INFO):
+    """
+    Initialize a logger with a stream handler and a file handler
+    
+    Args:
+      name (str): The name of the logger.
+      logdir: The directory where the log file will be saved.
+      level: The logging level. You can choose from: logging.DEBUG/INFO/WARNING/ERROR/CRITICAL
+    
+    Returns:
+      A logger
+    """
     if name != "":
         logger = logging.getLogger(name)
     else:
